9d29abae52b7a5b5fb31c3df3af70295c0a3348e
Ha Young Roh <hyroh795@gmail.com>
2022-11-14 19:40:19 -0500

    commit tree cb303bb8443a34433404d148829023fe505b8bd8

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(record) {
+  const wonYear = record.find(isWon);
+  if (wonYear != undefined) {
+    return wonYear.year;
+  }else {
+    return undefined;
+  }
+
+}
+
+function isWon(won) {
+    return won.result ==="W";
+}
